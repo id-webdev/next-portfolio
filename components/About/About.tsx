80c5fe27735a0f2357dbd76eddb65aedea6d4053
id-webdev <ilyuha45@gmail.com>
2023-01-14 15:32:58 +0100

    About: Removed unnecessary <code> tag and adjusted description

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -19,122 +19,118 @@ export default function About({ setCurrentSection }: AboutProps) {
           <div className="text-center">
             <h2 className="heading">#about</h2>
           </div>
-          <code>
-            <div className={styles.code}>
-              <div className={styles.codeRow}>
-                <span className={styles.codeLine}>1</span>
-                <div className={styles.codeText}>
-                  <OpenTag tag="section" />
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab2}`}>
-                <span className={styles.codeLine}>2</span>
-                <div className={styles.codeText}>
-                  <OpenTag tag="h2" />
-                  <span>About Me</span>
-                  <CloseTag tag="h2" />
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab2}`}>
-                <span className={styles.codeLine}>3</span>
-                <div className={styles.codeText}>
-                  <OpenTag tag="p" />
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab4}`}>
-                <span className={styles.codeLine}>4</span>
-                <div className={styles.codeText}>
-                  My journey began in the Summer of 2020 when I discovered my
-                  passion for Web Development. I started by creating basic
-                  landing pages using HTML, CSS &#40;SCSS&#41;, JavaScript,
-                  jQuery, and Bootstrap.
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab2}`}>
-                <span className={styles.codeLine}>5</span>
-                <div className={styles.codeText}>
-                  <CloseTag tag="p" />
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab2}`}>
-                <span className={styles.codeLine}>6</span>
-                <div className={styles.codeText}>
-                  <OpenTag tag="p" />
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab4}`}>
-                <span className={styles.codeLine}>7</span>
-                <div className={styles.codeText}>
-                  Through freelancing, I gained experience working on
-                  medium-scale projects in a team environment, which taught me
-                  the importance of client communication, understanding their
-                  requirements, and delivering projects on time.
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab4}`}>
-                <span className={styles.codeLine}>8</span>
-                <div className={styles.codeText}>
-                  <OpenTag tag="br" />
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab4}`}>
-                <span className={styles.codeLine}>9</span>
-                <div className={styles.codeText}>
-                  In those projects, I was responsible for converting Figma
-                  designs into responsive layouts, interfaces, and components
-                  while keeping performance, reusability, and scalability in
-                  mind.
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab2}`}>
-                <span className={styles.codeLine}>10</span>
-                <div className={styles.codeText}>
-                  <CloseTag tag="p" />
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab2}`}>
-                <span className={styles.codeLine}>11</span>
-                <div className={styles.codeText}>
-                  <OpenTag tag="p" />
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab4}`}>
-                <span className={styles.codeLine}>12</span>
-                <div className={styles.codeText}>
-                  To further improve my development skills, I delved into
-                  learning JavaScript frameworks. React & Vue have allowed me to
-                  make web development more efficient and enjoyable, and I
-                  continue to expand my knowledge of them through daily
-                  practice.
-                </div>
-              </div>
-
-              <div className={`${styles.codeRow} ${styles.tab2}`}>
-                <span className={styles.codeLine}>13</span>
-                <div className={styles.codeText}>
-                  <CloseTag tag="p" />
-                </div>
-              </div>
-
-              <div className={styles.codeRow}>
-                <span className={styles.codeLine}>14</span>
-                <div className={styles.codeText}>
-                  <CloseTag tag="section" />
-                </div>
+          <div className={styles.code}>
+            <div className={styles.codeRow}>
+              <span className={styles.codeLine}>1</span>
+              <div className={styles.codeText}>
+                <OpenTag tag="section" />
               </div>
             </div>
-          </code>
+
+            <div className={`${styles.codeRow} ${styles.tab2}`}>
+              <span className={styles.codeLine}>2</span>
+              <div className={styles.codeText}>
+                <OpenTag tag="h2" />
+                <span>About Me</span>
+                <CloseTag tag="h2" />
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab2}`}>
+              <span className={styles.codeLine}>3</span>
+              <div className={styles.codeText}>
+                <OpenTag tag="p" />
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab4}`}>
+              <span className={styles.codeLine}>4</span>
+              <div className={styles.codeText}>
+                My journey began in the Summer of 2020 when I discovered my
+                passion for Web Development. I started by creating landing pages
+                using HTML, CSS &#40;SCSS&#41;, JavaScript, jQuery, and
+                Bootstrap.
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab2}`}>
+              <span className={styles.codeLine}>5</span>
+              <div className={styles.codeText}>
+                <CloseTag tag="p" />
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab2}`}>
+              <span className={styles.codeLine}>6</span>
+              <div className={styles.codeText}>
+                <OpenTag tag="p" />
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab4}`}>
+              <span className={styles.codeLine}>7</span>
+              <div className={styles.codeText}>
+                Through freelancing, I gained experience working on medium-scale
+                projects in a team environment, which taught me the importance
+                of client communication, understanding their requirements, and
+                delivering projects on time.
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab4}`}>
+              <span className={styles.codeLine}>8</span>
+              <div className={styles.codeText}>
+                <OpenTag tag="br" />
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab4}`}>
+              <span className={styles.codeLine}>9</span>
+              <div className={styles.codeText}>
+                In those projects, I was responsible for converting Figma
+                designs into responsive layouts, interfaces, and components
+                while keeping performance, reusability, and scalability in mind.
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab2}`}>
+              <span className={styles.codeLine}>10</span>
+              <div className={styles.codeText}>
+                <CloseTag tag="p" />
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab2}`}>
+              <span className={styles.codeLine}>11</span>
+              <div className={styles.codeText}>
+                <OpenTag tag="p" />
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab4}`}>
+              <span className={styles.codeLine}>12</span>
+              <div className={styles.codeText}>
+                To further improve my development skills, I delved into learning
+                JavaScript frameworks. React & Vue have allowed me to make web
+                development more efficient and enjoyable, and I continue to
+                expand my knowledge of them through daily practice.
+              </div>
+            </div>
+
+            <div className={`${styles.codeRow} ${styles.tab2}`}>
+              <span className={styles.codeLine}>13</span>
+              <div className={styles.codeText}>
+                <CloseTag tag="p" />
+              </div>
+            </div>
+
+            <div className={styles.codeRow}>
+              <span className={styles.codeLine}>14</span>
+              <div className={styles.codeText}>
+                <CloseTag tag="section" />
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </Waypoint>
